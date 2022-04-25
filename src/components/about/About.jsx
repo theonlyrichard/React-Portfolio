1f67cd6bce6657a5a32fa5da5dbe12f5fde16c61
theonlyrichard <richard.cardoza16@gmail.com>
2022-04-24 22:48:14 -0700

    description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,11 @@ const About = () => {
                 </div>
 
             </div>
-            <div className="a-right"></div>
+            <div className="a-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub"> My name is Richard! Im currently a Insurance Agent and have made the desicion to make a career change to become a Software Developer.</p>
+                <p className="a-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptatum doloremque nisi maiores ipsum, culpa consequuntur expedita commodi possimus accusantium distinctio facilis alias, necessitatibus consequatur nesciunt fugit quod ducimus accusamus!</p>
+            </div>
 
 
         </div>
